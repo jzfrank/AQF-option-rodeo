37a40be15e04189b55c51184a0f57f1dbe728fcb
jzfrank <frank.jin.zhang@outlook.com>
2022-05-01 22:43:15 +0200

    change ml_predictor to ridge

--- ml_predictor.py
+++ ml_predictor.py
@@ -53,67 +53,72 @@ if __name__ == "__main__":
     #   Try using .loc[row_indexer,col_indexer] = value instead"
     pd.options.mode.chained_assignment = None 
 
-    mean_squared_errors = []
-    r2_scores = []
-    R_squared_OSXS_s = []
+    def run_regression(reg, reg_name):
+        mean_squared_errors = []
+        r2_scores = []
+        R_squared_OSXS_s = []
 
-    for year in range(1996, 2020 - 7):
-        start = time.time()
-        print(f"iteration, year: {year}, running regression...")
-        training_data, validation_data, test_data = train_validation_test_split(option_with_feature, year)
+        for year in range(1996, 2020 - 7):
+            start = time.time()
+            print(f"iteration, year: {year}, running regression...")
+            training_data, validation_data, test_data = train_validation_test_split(option_with_feature, year)
 
-        used_characteristics = ['volume', 'ReturnSkew', 'MaxRet', 'delta', 'PriceDelaySlope', 'strike_price', 
-                                'IdioVol3F', 'ReturnSkew3F', 'ir_rate', 'mid_price', 'forwardprice', 
-                                'zerotradeAlt1', 'theta', 'cfadj', 'zerotrade', 'best_bid', 'spotprice', 'VolMkt', 
-                                'IdioRisk', 'days_to_exp', 'PriceDelayTstat', 'High52', 'Coskewness', 'BidAskSpread', 
-                                'Beta', 'days_no_trading', 'open_interest', 'impl_volatility', 'PriceDelayRsq', 'IdioVolAHT', 
-                                'adj_spot', 'vega', 'gamma', 'best_offer', 'DolVol', 'cp_flag_encoded']
+            used_characteristics = ['volume', 'ReturnSkew', 'MaxRet', 'delta', 'PriceDelaySlope', 'strike_price', 
+                                    'IdioVol3F', 'ReturnSkew3F', 'ir_rate', 'mid_price', 'forwardprice', 
+                                    'zerotradeAlt1', 'theta', 'cfadj', 'zerotrade', 'best_bid', 'spotprice', 'VolMkt', 
+                                    'IdioRisk', 'days_to_exp', 'PriceDelayTstat', 'High52', 'Coskewness', 'BidAskSpread', 
+                                    'Beta', 'days_no_trading', 'open_interest', 'impl_volatility', 'PriceDelayRsq', 'IdioVolAHT', 
+                                    'adj_spot', 'vega', 'gamma', 'best_offer', 'DolVol', 'cp_flag_encoded']
 
-        imp = SimpleImputer(missing_values=np.nan, strategy='mean')
-        imp.fit(training_data[used_characteristics])
-        training_data.loc[:, used_characteristics] = imp.transform(training_data[used_characteristics])
-        validation_data.loc[:, used_characteristics] = imp.transform(validation_data[used_characteristics])
-        test_data.loc[:, used_characteristics] = imp.transform(test_data[used_characteristics])
+            imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+            imp.fit(training_data[used_characteristics])
+            training_data.loc[:, used_characteristics] = imp.transform(training_data[used_characteristics])
+            validation_data.loc[:, used_characteristics] = imp.transform(validation_data[used_characteristics])
+            test_data.loc[:, used_characteristics] = imp.transform(test_data[used_characteristics])
 
-        X_train = training_data[used_characteristics + ["date_x", "optionid"]]
-        y_train = training_data['option_ret']
+            X_train = training_data[used_characteristics + ["date_x", "optionid"]]
+            y_train = training_data['option_ret']
 
-        X_val = validation_data[used_characteristics + ["date_x", "optionid"]]
-        y_val = validation_data['option_ret']
+            X_val = validation_data[used_characteristics + ["date_x", "optionid"]]
+            y_val = validation_data['option_ret']
 
-        X_test = test_data[used_characteristics + ["date_x", "optionid"]]
-        y_test = test_data['option_ret']
+            X_test = test_data[used_characteristics + ["date_x", "optionid"]]
+            y_test = test_data['option_ret']
 
-        # # tuning hyperparameter on validation set
-        # # it costs a long time, so we just select an empirical result later instead of 
-        # # running it every time
-        # best_alpha = 0.1
-        # best_r2_score = None
-        # for alpha in [0.01, 0.1, 1]:
-        #     reg = linear_model.Lasso(random_state=0, alpha=best_alpha)
-        #     reg.fit(X_train[used_characteristics], y_train)
-        #     y_pred = reg.predict(X_val[used_characteristics])
-        #     r2_score_ = r2_score(y_val, y_pred)
-        #     if best_r2_score is None or r2_score_ > best_r2_score:
-        #         best_alpha = alpha 
-        #         best_r2_score = r2_score_
-        # print(f"best_alpha: {best_alpha}")
+            # # tuning hyperparameter on validation set
+            # # it costs a long time, so we just select an empirical result later instead of 
+            # # running it every time
+            # best_alpha = 0.1
+            # best_r2_score = None
+            # for alpha in [0.01, 0.1, 1]:
+            #     reg = linear_model.Lasso(random_state=0, alpha=best_alpha)
+            #     reg.fit(X_train[used_characteristics], y_train)
+            #     y_pred = reg.predict(X_val[used_characteristics])
+            #     r2_score_ = r2_score(y_val, y_pred)
+            #     if best_r2_score is None or r2_score_ > best_r2_score:
+            #         best_alpha = alpha 
+            #         best_r2_score = r2_score_
+            # print(f"best_alpha: {best_alpha}")
 
-        # regression    
-        best_alpha = 0.1  # empirically result from validation
-        reg = linear_model.Lasso(random_state=0, alpha=best_alpha)
-        reg.fit(X_train[used_characteristics], y_train)
-        y_pred = reg.predict(X_test[used_characteristics])
-        mean_squared_errors.append(mean_squared_error(y_test, y_pred))
-        r2_scores.append(r2_score(y_test, y_pred))
-        R_squared_OSXS_s.append(R_squared_OSXS(y_test, y_pred))
-        print(year, mean_squared_errors[-1], r2_scores[-1], R_squared_OSXS_s[-1])
-        print(f"finished one iteration, used {time.time() - start} seconds")
-        print("------------------------------------------------------")
+            # regression    
+            # best_alpha = 0.1  # empirically result from validation
+            # reg = linear_model.Lasso(random_state=0, alpha=best_alpha)
+            reg.fit(X_train[used_characteristics], y_train)
+            y_pred = reg.predict(X_test[used_characteristics])
+            mean_squared_errors.append(mean_squared_error(y_test, y_pred))
+            r2_scores.append(r2_score(y_test, y_pred))
+            R_squared_OSXS_s.append(R_squared_OSXS(y_test, y_pred))
+            print(year, mean_squared_errors[-1], r2_scores[-1], R_squared_OSXS_s[-1])
+            print(f"finished one iteration, used {time.time() - start} seconds")
+            print("------------------------------------------------------")
 
-    pd.DataFrame({
-        "start_year": list(range(1996, 2020 - 7)), 
-        "mean_squared_error": mean_squared_errors,
-        "r2_score": r2_scores,
-        "R_squared_OSXS": R_squared_OSXS_s
-    }).to_csv("Lasso_alpha0.1.csv")
+        pd.DataFrame({
+            "start_year": list(range(1996, 2020 - 7)), 
+            "mean_squared_error": mean_squared_errors,
+            "r2_score": r2_scores,
+            "R_squared_OSXS": R_squared_OSXS_s
+        }).to_csv(f"results/{reg_name}.csv")
+
+    best_alpha = 0.1  # empirically result from validation
+    reg = linear_model.Lasso(random_state=0, alpha=best_alpha)
+    run_regression(reg, "Ridge_alpha0.1")
